2a49ac4da2bfd59c5e960617e2f5003ec49ae51a
alinetmoraes <44126286+alinetmoraes@users.noreply.github.com>
2020-11-22 16:31:37 +0000

    Update ConsumeCalls.java

--- streams/microservices/util/ConsumeCalls.java
+++ streams/microservices/util/ConsumeCalls.java
@@ -13,12 +13,10 @@ public class ConsumeCalls {
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(final String[] args) {
         final Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-calls");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");       
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");    
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, Topics.calls.keySerde().deserializer().getClass());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, Topics.calls.valueSerde().deserializer().getClass());
         MonitoringInterceptorUtils.maybeConfigureInterceptorsConsumer(props);
